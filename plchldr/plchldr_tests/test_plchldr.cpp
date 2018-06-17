7dd37c491eb705bbc74adccff1b8c23f2a9ae861
ivan-aksamentov <ivan.aksamentov@gmail.com>
2018-06-17 18:23:07 +0200

    Add parameterized test boilerplate

--- plchldr/plchldr_tests/test_plchldr.cpp
+++ plchldr/plchldr_tests/test_plchldr.cpp
@@ -1,10 +1,35 @@
+#include <tuple>
+#include <vector>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <plchldr/plchldr.h>
 
+
+// Plain test
 TEST(Add, adds) {
   const auto actual = add(3, 2);
   const auto expected = 5;
   EXPECT_EQ(expected, actual);
 }
+
+// Parametrized test
+class AddWithParams :
+    public testing::TestWithParam<std::tuple<int, int, int>> {};
+
+TEST_P(AddWithParams, adds) {
+  int x, y, sum;
+  std::tie(x, y, sum) = GetParam();
+  EXPECT_EQ(sum, add(x, y));
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Add,
+    AddWithParams,
+    ::testing::Values(
+        // first arg, second arg, result
+        std::make_tuple(0, 0, 0),
+        std::make_tuple(2, 3, 5),
+        std::make_tuple(2, -3, -1)
+    ));
